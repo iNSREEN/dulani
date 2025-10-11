fa7bdc446eebd4c89c9c8b7ce5728a2352bb73ee
Nsreen Alfaifi <116817889+iNSREEN@users.noreply.github.com>
2025-10-11 20:01:48 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,14 +27,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 VERY IMPORTANT: add your repo name here
+  base: "/dulani/",  // 
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
